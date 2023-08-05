98c944985dc248234eeba665d957c187a4dddb61
Martin Bouillaud <mbouillaud@lefigaro.fr>
2023-08-05 11:06:48 +0200

    first commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export DOCKER_BUILDKIT=1
+
+docker buildx build --platform linux/amd64 -t martinbouillaud/inotify-ytdlp .
+
+if [[ $? -eq 0 ]]; then
+    docker tag martinbouillaud/inotify-ytdlp martinbouillaud/inotify-ytdlp:latest
+    docker tag martinbouillaud/inotify-ytdlp martinbouillaud/inotify-ytdlp:$1
+
+    docker push martinbouillaud/inotify-ytdlp:latest
+    docker push martinbouillaud/inotify-ytdlp:$1
+fi
