7cee8e0042ed3189f3699aaa2de7cb7beafc8562
Martin Bouillaud <mbouillaud@lefigaro.fr>
2023-09-28 12:18:35 +0200

    ff

--- build.sh
+++ build.sh
@@ -3,8 +3,11 @@
 export DOCKER_BUILDKIT=1
 
 docker buildx build --platform linux/amd64 -t martinbouillaud/inotify-ytdlp .
+docker buildx build --platform linux/arm64 -t martinbouillaud/inotify-ytdlp:arm64 .
 
 if [[ $? -eq 0 ]]; then
     docker tag martinbouillaud/inotify-ytdlp martinbouillaud/inotify-ytdlp:latest
+    docker tag martinbouillaud/inotify-ytdlp martinbouillaud/inotify-ytdlp:arm64
     docker push martinbouillaud/inotify-ytdlp:latest
+    docker push martinbouillaud/inotify-ytdlp:arm64
 fi
